9b6b56d40beeeedb6845edd32891432fc55c20cb
Aleksey Sitnikov <alexsitnikov2003@gmail.com>
2022-06-06 21:59:43 +0300

    init: start oaip 2 term 3 lab

---
+++ oaip-labs/2-term/3-lab/double-linked-list.h
@@ -0,0 +1,8 @@
+//
+// Created by asitnikoff on 6/6/2022.
+//
+
+#ifndef INC_3_LAB_DOUBLE_LINKED_LIST_H
+#define INC_3_LAB_DOUBLE_LINKED_LIST_H
+
+#endif //INC_3_LAB_DOUBLE_LINKED_LIST_H
---
+++ oaip-labs/2-term/3-lab/validation.h
@@ -0,0 +1,8 @@
+//
+// Created by asitnikoff on 6/6/2022.
+//
+
+#ifndef INC_3_LAB_VALIDATION_H
+#define INC_3_LAB_VALIDATION_H
+
+#endif //INC_3_LAB_VALIDATION_H
