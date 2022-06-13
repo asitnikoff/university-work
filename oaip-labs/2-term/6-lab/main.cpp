9ed7c8f0646af826c656a9f606dd5dd0c0f02d16
Aleksey Sitnikov <alexsitnikov2003@gmail.com>
2022-06-13 15:53:44 +0300

    feat: realize 6 lab

--- oaip-labs/2-term/6-lab/main.cpp
+++ oaip-labs/2-term/6-lab/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <cmath>
+
+
+double a, b, eps, h;
+
+
+double fun(double);
+double getZeroOfFunction(double, double, double);
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Input a: ";
+    fflush(stdout);
+    std::cin >> a;
+
+    std::cout << "Input b: ";
+    fflush(stdout);
+    std::cin >> b;
+
+    std::cout << "Input eps: ";
+    fflush(stdout);
+    std::cin >> eps;
+
+    std::cout << "Input h: ";
+    fflush(stdout);
+    std::cin >> h;
+
+    while (!((a + h) > b)) {
+        if ((fun(a) * fun(a + h)) < 0) {
+            std::cout << "zero of function: " << getZeroOfFunction(a, a + h, eps) << std::endl;
+        }
+
+        a += h;
+    }
+
     return 0;
+}
+
+
+double fun(double x) {
+    return (sin(x) * sin(x)) - (x / 5.0) - 1;
+}
+
+
+double getZeroOfFunction(double a, double b, double eps) {
+    double x0 = a, x1 = b, x2;
+    while (fabs(x0 - x1) > eps) {
+        x2 = x1 - ((x1 - x0) / (fun(x1) - fun(x0))) * fun(x1);
+
+        x0 = x1;
+        x1 = x2;
+    }
+
+    return x1;
 }
