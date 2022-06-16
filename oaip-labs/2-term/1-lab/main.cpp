a01ca555ecabf5693dc49d4e56d7c64359422d91
Aleksey Sitnikov <alexsitnikov2003@gmail.com>
2022-06-16 19:35:59 +0300

    feat: add new features in 1 lab

--- oaip-labs/2-term/1-lab/main.cpp
+++ oaip-labs/2-term/1-lab/main.cpp
@@ -7,35 +7,52 @@
 template <typename Type>
 Type read();
 void clearInputBuffer();
+void inputParameters(double&, int&, bool&);
 
 
 int main() {
+    bool is_parameters_entered = false;
+    double x;
+    int n;
+
+    std::cout << "This program calculates x^n." << std::endl;
+
     while (true) {
-        std::cout << "Input x:" << std::endl;
-        int x = read<int>();
-
-        std::cout << "Input n:" << std::endl;
-        int n = read<int>();
-
+        std::cout << "Input paramaters - 0" << std::endl;
         std::cout << "Recursion binary pow - 1" << std::endl;
         std::cout << "Simple pow - 2" << std::endl;
         std::cout << "Iterative binary pow - 3" << std::endl;
+        std::cout << "Print all - 4" << std::endl;
         std::cout << "Exit - another key" << std::endl;
         std::cout << "Choose option: ";
-        char choice;
+
+        int choice;
         std::cin >> choice;
+
+        if (!is_parameters_entered && (choice >= 1) && (choice <= 4)) {
+            std::cout << "You must input parameters first!";
+            inputParameters(x, n, is_parameters_entered);
+        }
 
         switch (choice) {
             default:
-                system("pause");
+                std::cout << "Bye!\n";
                 return 0;
-            case '1':
+            case 0:
+                inputParameters(x, n, is_parameters_entered);
+                break;
+            case 1:
                 std::cout << "Recursion result = " << calcRecursionBinPow(x, n) << "\n";
                 break;
-            case '2':
+            case 2:
                 std::cout << "Simple result = " << calcSimplePow(x, n) << "\n";
                 break;
-            case '3':
+            case 3:
+                std::cout << "Iterative result = " << calcIterativeBinPow(x, n) << "\n";
+                break;
+            case 4:
+                std::cout << "Recursion result = " << calcRecursionBinPow(x, n) << "\n";
+                std::cout << "Simple result = " << calcSimplePow(x, n) << "\n";
                 std::cout << "Iterative result = " << calcIterativeBinPow(x, n) << "\n";
                 break;
         }
@@ -48,9 +65,6 @@ template <typename Type>
 Type read() {
     Type x;
     while (true) {
-//        std::cin.clear();
-//        std::cin.ignore(10, '\n');
-
         std::cin >> x;
         if (std::cin.get() != '\n') {
             clearInputBuffer();
@@ -67,4 +81,15 @@ Type read() {
 void clearInputBuffer() {
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+
+void inputParameters(double& x, int& n, bool& is_parameters_entered) {
+    std::cout << "Input x: ";
+    x = read<double>();
+
+    std::cout << "Input n: ";
+    n = read<int>();
+
+    is_parameters_entered = true;
 }
--- oaip-labs/2-term/7-lab/main.cpp
+++ oaip-labs/2-term/7-lab/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+
+int n, m;
+double a, b;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "Input a: ";
+    fflush(stdout);
+    std::cin >> a;
+
+    std::cout << "Input b: ";
+    fflush(stdout);
+    std::cin >> b;
+
+    std::cout << "Input m: ";
+    fflush(stdout);
+    std::cin >> m;
+
+    std::cout << "Input n: ";
+    fflush(stdout);
+    std::cin >> n;
+
+    double h1 = (b - a) / m;
+    double h2 = (b - a) / n;
+
+    double x1 = a;
+    double x2 = a;
+
+    while (!(x2 > b)) {
+        
+
+        x2 += h2;
+        while ((x1 + (2. * h1)) < x2) {
+            x1 += h1;
+        }
+    }
 }
